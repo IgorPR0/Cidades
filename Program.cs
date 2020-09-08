342131bd03fa689526d100cc98cb899bd5498cd6
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-08 15:12:11 -0300

    Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guarujá ,Santos, São Vicente");
+            Console.WriteLine("Praia Grande, Mongaguá, Itanhaém");
+            Console.WriteLine("Peruíbe, Iguape, Ilha Comprida e Cananéia.");
+        }
+    }
+}
